2681eaebbea22f53253247858cd0d92e7b7b7155
JL Reyes <joseluisreyes43@gmail.com>
2024-01-29 06:58:45 +0800

    made the navbar nicely green

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-gray-800 p-6 text-white flex justify">
+  <nav class="custom-navbar p-6 text-white flex justify">
     <router-link to="/" class="px-4 hover:bg-gray-700 py-2 rounded">Home</router-link>
     <router-link to="/general-board" class="px-4 hover:bg-gray-700 py-2 rounded">General Learning Board</router-link>
     <router-link to="/otc" class="px-4 hover:bg-gray-700 py-2 rounded">EMEA S/4HANA for OTC Beginner</router-link>
@@ -7,12 +7,9 @@
   </nav>
 </template>
 
-
 <style>
-nav {
+nav.custom-navbar {
+  background-color: #0f4f07;
   font-family: 'Roboto', sans-serif;
 }
-</style>
-
-<style>
 </style>
